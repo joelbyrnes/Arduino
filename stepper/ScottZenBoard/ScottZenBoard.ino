80d830e551e57604d986a3e9a20a28b329c0e933
Joel Byrnes <fatal.exception.occurred@gmail.com>
2015-09-27 15:37:30 +1000

    using a struct for stepper stuff

--- stepper/ScottZenBoard/ScottZenBoard.ino
+++ stepper/ScottZenBoard/ScottZenBoard.ino
@@ -8,15 +8,7 @@ D :  6   7   8   3   4   5   6   (repeat)   39
 E :  7   8   3   4   5   6   7   (repeat)   40
 F :  8   3   4   5   6   7   8   (repeat)   41 
 
-move 90 degrees after x minutes. 
-
-p0    p1    p2    p3    p0    p1    p2    p3     p0?
-
-A :  3     4     5     6     7     8     3 
-      t=3   t=7   t=12  t=18  t=25  t=33  t=36
-B :  4     5     6     7     8     3     4 
-      t=4   t=9   t=15  t=22  t=30  t=33  t=37
-etc
+Move 90 degrees after x seconds/minutes. 
 
 TO DO:
 * pause switch so boards can be fixed up
@@ -60,8 +52,6 @@ TO DO:
 const int sensorPin = A1;    // pin that the sensor is attached to
 const int ledPin = 8;
 
-#define MAX_SPEED  1200.0     // steps per second
-
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
 // HALF4WIRE or FULL4WIRE
 AccelStepper stepperA(AccelStepper::HALF4WIRE, motorPinA1, motorPinA3, motorPinA2, motorPinA4);
@@ -72,11 +62,11 @@ AccelStepper stepperE(AccelStepper::HALF4WIRE, motorPinE1, motorPinE3, motorPinE
 AccelStepper stepperF(AccelStepper::HALF4WIRE, motorPinF1, motorPinF3, motorPinF2, motorPinF4);
 
 #define NUM_STEPPERS 6
-AccelStepper *steppers[NUM_STEPPERS] = {&stepperA, &stepperB, &stepperC, &stepperD, &stepperE, &stepperF};
 
 // according to http://42bots.com/tutorials/28byj-48-stepper-motor-with-uln2003-driver-and-arduino-uno/ 
 // this motor isn't quite 4096 steps, more like 4076. 1019 per 90 degrees.
 
+#define MAX_SPEED  1200.0     // steps per second
 int stepsPer90Degrees = 1019;
 
 const int lightThreshold = 100;
@@ -86,27 +76,25 @@ int stopFOnMark;
 int foundA;
 int foundF;
 
-// TODO remove?
-MilliTimer timer1;
-
 enum {SEQ_A, SEQ_B, SEQ_C, SEQ_D, SEQ_E, SEQ_F, PRINT_TIME, TASK_LIMIT};
-// task for homing? 
 
 Scheduler scheduler (TASK_LIMIT);
 
-const int sequenceA[] = {3, 4, 5, 6, 7, 8, 3};
-const int sequenceB[] = {4, 5, 6, 7, 8, 3, 4};
-const int sequenceC[] = {5, 6, 7, 8, 3, 4, 5};
-const int sequenceD[] = {6, 7, 8, 3, 4, 5, 6};
-const int sequenceE[] = {7, 8, 3, 4, 5, 6, 7};
-const int sequenceF[] = {8, 3, 4, 5, 6, 7, 8};
+struct StepperCluster {
+  AccelStepper *stepper;
+  const int sequence[7];
+  int seqPos;
+  int task;
+}; 
 
-int seqAPos = 0;
-int seqBPos = 0;
-int seqCPos = 0;
-int seqDPos = 0;
-int seqEPos = 0;
-int seqFPos = 0;
+StepperCluster clusterA { &stepperA, {3, 4, 5, 6, 7, 8, 3}, 0, SEQ_A };
+StepperCluster clusterB { &stepperB, {4, 5, 6, 7, 8, 3, 4}, 0, SEQ_B };
+StepperCluster clusterC { &stepperC, {5, 6, 7, 8, 3, 4, 5}, 0, SEQ_C };
+StepperCluster clusterD { &stepperD, {6, 7, 8, 3, 4, 5, 6}, 0, SEQ_D };
+StepperCluster clusterE { &stepperE, {7, 8, 3, 4, 5, 6, 7}, 0, SEQ_E };
+StepperCluster clusterF { &stepperF, {8, 3, 4, 5, 6, 7, 8}, 0, SEQ_F };
+
+StepperCluster *clusters[NUM_STEPPERS] = {&clusterA, &clusterB, &clusterC, &clusterD, &clusterE, &clusterF}; 
 
 // control how long the sequence times are, if they were in seconds. Note that scheduler works in 10ths of a second. 
 int timeFactor = 10;
@@ -118,22 +106,16 @@ void setup() {
   digitalWrite(ledPin, LOW);
   
   Serial.begin(57600); 
-
-  delay(100);
   
   for (int i=0; i < NUM_STEPPERS; i++) {
-    steppers[i]->setMaxSpeed(MAX_SPEED);
-    steppers[i]->setAcceleration(800.0);
-    steppers[i]->setSpeed(400);  
-  }
+    // configure steppers
+    clusters[i]->stepper->setMaxSpeed(MAX_SPEED);
+    clusters[i]->stepper->setAcceleration(800.0);
+    clusters[i]->stepper->setSpeed(400);  
 
-  // tenths of a second, ie 5 = 0.5s
-  scheduler.timer(SEQ_A, sequenceA[seqAPos++] * timeFactor);
-  scheduler.timer(SEQ_B, sequenceB[seqBPos++] * timeFactor);
-  scheduler.timer(SEQ_C, sequenceB[seqCPos++] * timeFactor);
-  scheduler.timer(SEQ_D, sequenceB[seqDPos++] * timeFactor);
-  scheduler.timer(SEQ_E, sequenceB[seqEPos++] * timeFactor);
-  scheduler.timer(SEQ_F, sequenceF[seqFPos++] * timeFactor);
+    // schedule movements in tenths of a second, ie 5 = 0.5s
+    scheduler.timer(clusters[i]->task, clusters[i]->sequence[clusters[i]->seqPos++] * timeFactor);
+  }
   
   scheduler.timer(PRINT_TIME, 10);
 
@@ -166,67 +148,67 @@ void loop() {
   stepperF.run();
 }
 
-void schedule(AccelStepper &stepper, int task, const int sequence[], int seqPos) {
+void schedule(struct StepperCluster &cluster) {
   // schedule again according to sequence
-  scheduler.timer(task, sequence[seqPos % 7] * timeFactor);
-  stepper.enableOutputs();
-  stepper.setMaxSpeed(MAX_SPEED);
-  stepper.move(stepsPer90Degrees);
+  scheduler.timer(cluster.task, cluster.sequence[cluster.seqPos++ % 7] * timeFactor);
+  cluster.stepper->enableOutputs();
+  cluster.stepper->setMaxSpeed(MAX_SPEED);
+  cluster.stepper->move(stepsPer90Degrees);
 }
 
 void doWhatNow() {
   // TODO repeat for all steppers
   //homing();
   
+  // save power by turning off outputs when not moving
   for (int i=0; i < NUM_STEPPERS; i++) {
-    if (steppers[i]->distanceToGo() == 0) {
-      steppers[i]->disableOutputs();
+    if (clusters[i]->stepper->distanceToGo() == 0) {
+      clusters[i]->stepper->disableOutputs();
     }
   }
   
   switch(scheduler.poll()) {
     case SEQ_A: 
       Serial.print("SEQ_A pos = ");
-      Serial.println(seqAPos);
+      Serial.println(clusterA.seqPos);
       
-      schedule(stepperA, SEQ_A, sequenceA, seqAPos++); 
+      schedule(clusterA); 
       //stopAOnMark = 1;
       break;
 
     case SEQ_B: 
       Serial.print("SEQ_B pos = ");
-      Serial.println(seqBPos);
+      Serial.println(clusterB.seqPos);
       
-      schedule(stepperB, SEQ_B, sequenceB, seqBPos++); 
+      schedule(clusterB); 
       break;
 
     case SEQ_C: 
       Serial.print("SEQ_C pos = ");
-      Serial.println(seqCPos);
+      Serial.println(clusterC.seqPos);
       
-      schedule(stepperC, SEQ_C, sequenceC, seqCPos++); 
+      schedule(clusterC); 
       break;
 
     case SEQ_D: 
       Serial.print("SEQ_D pos = ");
-      Serial.println(seqDPos);
+      Serial.println(clusterD.seqPos);
       
-      schedule(stepperD, SEQ_D, sequenceD, seqDPos++); 
+      schedule(clusterD); 
       break;
 
     case SEQ_E: 
       Serial.print("SEQ_E pos = ");
-      Serial.println(seqEPos);
+      Serial.println(clusterE.seqPos);
       
-      schedule(stepperE, SEQ_E, sequenceE, seqEPos++); 
+      schedule(clusterE); 
       break;
 
     case SEQ_F: 
       Serial.print("SEQ_F pos = ");
-      Serial.println(seqFPos);
+      Serial.println(clusterF.seqPos);
       
-      // schedule again according to sequence
-      schedule(stepperF, SEQ_F, sequenceF, seqFPos++); 
+      schedule(clusterF); 
       break;
 
     case PRINT_TIME: 
