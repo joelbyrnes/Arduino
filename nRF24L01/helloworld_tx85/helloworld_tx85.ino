832f5ee0f2be365d14be27480727d8777efc6137
Joel Byrnes <fatal.exception.occurred@gmail.com>
2015-11-28 20:33:10 +1000

    recovered working helloworld_tx85 sketch with multi-MCU support and radio detection

--- nRF24L01/helloworld_tx85/helloworld_tx85.ino
+++ nRF24L01/helloworld_tx85/helloworld_tx85.ino
@@ -19,18 +19,30 @@
 #include <EEPROM.h>
 
 #define DEBUG_OUTPUT
-//#define CHECK_RADIO
+#define CHECK_RADIO
 
-// pins
-#if defined(__AVR_ATtiny84__) // #if defined( __AVR_ATtinyX4__ )
+// pins for each type of microcontroller
+#if defined(__AVR_ATtiny84__) // or #if defined( __AVR_ATtinyX4__ )
+  #define MCU "ATtiny84"
   #define cePin 2
-  #define csnPin 5
-#elif defined(__AVR_ATtiny85__) // #if defined( __AVR_ATtinyX5__ )
+  #define csnPin 3
+  #define txPin 1
+  #define rxPin 0
+  #define LED_PIN 8 
+#elif defined(__AVR_ATtiny85__) // or #if defined( __AVR_ATtinyX5__ )
+  #define MCU "ATtiny85"
   #define cePin 5
   #define csnPin 4
+  //#define txPin 1
+  //#define rxPin 0
+  #define LED_PIN 8 
 #elif defined(__AVR_ATmega328P__)
+  #define MCU "ATmega328P"
   #define cePin 9
   #define csnPin 10
+  #define LED_PIN 8
+  #define txPin 1
+  #define rxPin 0
 #else
 // ???
 #endif
@@ -42,12 +54,12 @@ RF24 radio(cePin, csnPin);
 RF24Network network(radio);
 
 // Address of our node
-const uint16_t this_node = EEPROM.read(0); 
+uint16_t this_node;
 
 // Address of the other node
 const uint16_t other_node = 0;
 
-const uint16_t channel = 80;
+const uint16_t channel = 90;
 
 // How often to send 'hello world to the other unit
 const unsigned long interval = 2000; //ms
@@ -67,34 +79,49 @@ struct payload_t
 
 void setup(void)
 {
-  // too hard to set up Rx pin - keep this commented out
-  //EEPROM.write(0, 1);
-  
-  #if defined(DEBUG_OUTPUT)
-  Serial.begin(115200);
-  Serial.println("helloworld_tx85");
-  #endif
+  // convenience to change node ID - keep this commented out
+  EEPROM.write(0, 3);
+
+  // wait for things to start?
+  //delay(1000);
  
+  pinMode(LED_PIN, OUTPUT);     
+  digitalWrite(LED_PIN, LOW);  
+
   SPI.begin();
   radio.begin();
-  network.begin(channel, /*node address*/ this_node);
-  
+  radio.enableDynamicPayloads();
+  radio.setAutoAck(true);
+  radio.powerUp();
+    
+  this_node = EEPROM.read(0); 
+
   #if defined(DEBUG_OUTPUT)
-  Serial.print("channel: ");
+  Serial.begin(115200);
+  Serial.print("helloworld_tx85 ");
+  Serial.println(MCU);
+  Serial.print("ch: ");
   Serial.print(channel);
-  Serial.print(", this_node: ");
+  Serial.print(", node: ");
   Serial.println(this_node);
   #endif  
-    
-  #if defined(CHECK_RADIO) && defined(DEBUG_OUTPUT)
-  Serial.println("Checking radio... ");
+
+  #if defined(CHECK_RADIO) 
   if (radioConnected()) {
-    Serial.println("Radio ready.");
+    #if defined(DEBUG_OUTPUT)
+    Serial.println("+++ Radio ready.");
+    #endif 
   } else {
-    // probably any one of CSN (normally 10), MOSI, MISO or SCK (11, 12, 13) are not connected correctly 
-    Serial.println("Could not read from radio.");
+    // probably any one of CSN (normally 10), MOSI, MISO or SCK (11, 12, 13) are not connected correctly
+    #if defined(DEBUG_OUTPUT) 
+    Serial.println("--- No radio.");
+    #endif 
+    digitalWrite(LED_PIN, HIGH);  
+    delay(100);
   }
   #endif  
+  
+  network.begin(channel, this_node);
 }
 
 void loop(void)
@@ -113,12 +140,34 @@ void loop(void)
     //Serial.print("Sending...");
 #endif
     payload_t payload = { millis(), packets_sent++ };
+    
     RF24NetworkHeader header(/*to node*/ other_node);
-    bool ok = network.write(header,&payload,sizeof(payload));
+    
 #if defined(DEBUG_OUTPUT)
-    if (ok) Serial.println("ok.");
-    else Serial.println("failed.");
+    Serial.print("tx ");
+    Serial.print(payload.ms);
+    Serial.print(", ");
+    Serial.print(payload.counter);
+    Serial.print("; ");
+    Serial.print(this_node);
+    Serial.print(" -> ");
+    Serial.println(other_node);
 #endif
+
+    bool ok = network.write(header,&payload,sizeof(payload));
+
+    if (ok) {
+//      last_sent = now;
+      digitalWrite(LED_PIN, LOW);  
+#if defined(DEBUG_OUTPUT)
+      Serial.println("ok");
+#endif
+    } else {
+      digitalWrite(LED_PIN, HIGH);  
+#if defined(DEBUG_OUTPUT)
+      Serial.println("no");
+#endif
+    }
   }
 }
 
