18a133f219acc5a8a4e33660c65a46c628f4ca86
Joel Byrnes <fatal.exception.occurred@gmail.com>
2019-03-24 19:00:13 +1000

    simplify to single input

--- Infrared/IRSensorLEDStrip/IRSensorLEDStrip.ino
+++ Infrared/IRSensorLEDStrip/IRSensorLEDStrip.ino
@@ -2,27 +2,13 @@
 
 #include "FastLED.h"
 
+#define SENSOR_PIN A0
 #define CONTROL_PIN 11
-
 #define NUM_LEDS 170
 
 CRGB leds[NUM_LEDS];
 
-struct Input {
-  int inputPin;    // voltage-divider circuit attached to analog pin X
-  int pwmPin;      // pin to output PWM on
-  int raw;         // analog value read 
-  
-  int note;        // MIDI note value in hex eg middle C == 0x48
-  int velocity;    // track whether note is off
-}; 
-
-Input inputA { A0, 3, 0, 0x39, 0 }; // A
-
-#define NUM_INPUTS 1
-Input *inputs[NUM_INPUTS] = { &inputA }; 
-
-// TODO use define for pins
+// TODO use more defines
 
 unsigned long time = 0;         
 int ledValue = 0;
@@ -56,55 +42,42 @@ void setup() {
 int h_readings[NUM_SAMPLES];      // the readings from the analog input
 int h_index = 0;                  // the index of the current reading
 int h_total = 0;                  // the running total
+unsigned int smoothed = 0;
 
 void loop() {
-  // read all analog inputs at once
-  for (int i=0; i < NUM_INPUTS; i++) {
-    int raw = analogRead(inputs[i]->inputPin);
-    // smooth input
-    
-    // subtract the last reading:
-    h_total = h_total - h_readings[h_index];         
-    // read from the sensor:  
-    h_readings[h_index] = raw; 
-    // add the reading to the total:
-    h_total = h_total + h_readings[h_index++];       
+  int raw = analogRead(SENSOR_PIN);
+  // smooth input
   
-    // if we're at the end of the array, restart
-    if (h_index >= NUM_SAMPLES) h_index = 0;                           
+  // subtract the last reading:
+  h_total = h_total - h_readings[h_index];         
+  // read from the sensor:  
+  h_readings[h_index] = raw; 
+  // add the reading to the total:
+  h_total = h_total + h_readings[h_index++];       
+
+  // if we're at the end of the array, restart
+  if (h_index >= NUM_SAMPLES) h_index = 0;                           
+
+  // calculate the average:
+  smoothed = h_total / NUM_SAMPLES;
   
-    // calculate the average:
-    inputs[i]->raw = h_total / NUM_SAMPLES;
-    
-    //inputs[i]->raw = raw;
-    if (inputs[i]->raw < rawMin) rawMin = inputs[i]->raw;
-    if (inputs[i]->raw > rawMax) rawMax = inputs[i]->raw;   // never decrease? maybe over time? 
-  }
+  ledValue = map(smoothed, rawMin, rawMax, 0, 255);
+  //avg = int(float(avg) * 0.99 + float(ledValue) * 0.01);
+  corrected = constrain(ledValue, 0, 255); // values are constrained to ensure it is in map, plus will not turn off outside range
   
-  // process inputs
-  for (int i=0; i < NUM_INPUTS; i++) {
-    ledValue = map(inputs[i]->raw, rawMin, rawMax, 0, 255);
-    //avg = int(float(avg) * 0.99 + float(ledValue) * 0.01);
-    corrected = constrain(ledValue, 0, 255); // values are constrained to ensure it is in map, plus will not turn off outside range
-    
-    analogWrite(inputs[i]->pwmPin, corrected);  
-    
-    if (millis() > ledTime + ledInterval) {
-      ledTime = millis(); 
-      
-      if (1) { // ledValue >= 10
-        // push array along by 1, and set the new value to the 1st 
-        memmove8(&leds[1], &leds[0], (NUM_LEDS - 1) * sizeof(struct CRGB));
-        leds[0].setRGB(0, corrected, 0);
-        FastLED.show();
-      }
-    }
+  if (millis() > ledTime + ledInterval) {
+    ledTime = millis(); 
+
+    // push array along by 1, and set the new value to the 1st 
+    memmove8(&leds[1], &leds[0], (NUM_LEDS - 1) * sizeof(struct CRGB));
+    leds[0].setRGB(0, corrected, 0);
+    FastLED.show();
   }
   
   if (millis() > time + 100) {
     time = millis(); 
     Serial.print("Value: ");    
-    Serial.print(inputs[0]->raw);  
+    Serial.print(smoothed);  
     Serial.print(" - min: ");    
     Serial.print(rawMin);  
     Serial.print(" - max: ");    
